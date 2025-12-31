bc1604ac359e4f5debd6a1385e99a91f8cf24931
Vagish Hemmige <vahemmig@montefiore.org>
2025-12-31 17:47:31 -0500

    Create main analysis file

---
+++ R/main.R
@@ -0,0 +1,6 @@
+#This is the main analysis file for the cryptococcus CEA, calling the other files in the appropriate order
+
+source("R/setup.R")
+source("R/functions.R")
+source("R/create_diagram_QC.R")
+source("R/create_sensitivity_analysis_QC.R")
